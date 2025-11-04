73b3ba39c6061b36960654cb1220c8f41067d1f2
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2025-11-04 20:30:48 +0700

    update_modelBusstop

--- src/models/busStop.js
+++ src/models/busStop.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
     }, {
         sequelize,
         modelName: 'busStop',
-        tableName: 'busStop',
+        tableName: 'busstop',
         timestamps: true,
         underscored: false
     });
